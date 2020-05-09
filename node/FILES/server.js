1bd6a9c614508c238ac0c28db88be801c498d9ca
Lucas Telles <ltellesfl@gmail.com>
2020-05-08 21:35:09 -0300

    Alterando mysql host, movendo arquivos e criando Dockerfile para o node js

--- node/FILES/server.js
+++ node/FILES/server.js
@@ -7,7 +7,7 @@ if (!port) {
     port = 8888;
 }
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'database',
     user: 'user_packages',
     password: 'passwords'
 });
