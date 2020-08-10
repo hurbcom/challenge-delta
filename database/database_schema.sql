7f90de25371fd9b47bd2dba0e9546e61e885c4b9
João Ferreira <joao.ferreira@petz.com.br>
2020-08-10 00:09:28 -0300

    chore (proxy): criado dockerfile para o database

---
+++ database/database_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `packages`.`offer` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Text` varchar(255) NOT NULL,
+  `CreateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
