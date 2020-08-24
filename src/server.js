c0065149df075b72693501e30f06dba393547d7f
Julliano Gonçalves <julliano_marcus@hotmail.com>
2020-08-23 22:13:48 -0300

    Improvements to the API project code.
    * Flag included to check the version of the project.
      - This flag was added so that in the process of building the image, a small check can be made if the code was packaged correctly.
    * Support for environment variables to set connection values with the database.
      - The goal is that when executed in docker-compose or kubernetes, the values for connection to the database are passed in a minimally safe way.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 var restify = require('restify');
 var errors = require('restify-errors');
 var mysql = require('mysql');
+var pjson = require('./package.json');
 
 var port = process.env.NODEJS_API_PORT;
 if (!port) {
     port = 8888;
 }
+
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'user_packages',
-    password: 'passwords'
+    host: process.env.MYSQL_HOST != undefined ? process.env.MYSQL_HOST : 'locahost',
+    user: process.env.MYSQL_USER != undefined ? process.env.MYSQL_USER : 'user_packages',
+    password: process.env.MYSQL_PASSWORD != undefined ? process.env.MYSQL_PASSWORD : 'passwords'
 });
 
 var server = restify.createServer();
@@ -29,16 +31,16 @@ server.post('/packages', function (request, response, next) {
     if(!request.body) { return next(new errors.BadRequestError("texto inválido")); }
     connection.query('insert into packages.offer (Text) values ("?")', [request.body], function (error, results, fields) {
         if (error) { next(error); return; }
-        response.end("Ok");
+        response.end("Ok. Id: " + results.insertId);
     });
 });
 
 // delete packages
 server.del('/packages/:id', function (request, response, next) {
     var id = request.params.id;
-    
+
     if(!id || id <= 0) { return next(new errors.BadRequestError("id inválido")); }
-    
+
     connection.query('delete from packages.offer WHERE Id=?', [id], function (error, results, fields) {
         if (error) { next(error); return; }
         if(!results.affectedRows) { next(new errors.BadRequestError("id inválido")); return; }
@@ -51,3 +53,8 @@ server.listen(port, function () {
 });
 
 module.exports = server;
+
+if (process.argv.includes('--version')) {
+    console.log(pjson.version)
+    process.exit(0)
+}
