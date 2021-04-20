199b8258bafc9d9bf78bcc666141fecf5b8d08a9
CRF Luiz <lcmartinscjr@gmail.com>
2021-04-20 01:35:08 +0000

    finished gets

--- app/route/index.js
+++ app/route/index.js
@@ -1,14 +1,32 @@
+const ProductBLL = require('../controller/Products');
+const basicOptionsToApi = { htmlResponse: true };
+
+const internalError = (er, res) => {
+    console.log(er);
+    res.status(500).json({ errorText: "The server has an error!" })
+}
+
 module.exports = server => {
     server.get('/api/products', (req, res) => {
-        let { body, params, query } = req;
-        console.log({ body, params, query });
-        res.status(200).json({ ok: true, http: 'OK' });
+        try{
+            const product = new ProductBLL(basicOptionsToApi);
+            let productsList = product.showProducts(req.query);
+            res.status(productsList.status).json(productsList.response);
+        }catch(er){
+            internalError(er, res);
+        }
     });
     
     server.get('/api/products/:productId', (req, res) => {
-        let { body, params, query } = req;
-        console.log({ body, params, query });
-        res.status(200).json({ ok: true, http: 'OK' });
+        try{
+            const product = new ProductBLL(basicOptionsToApi);
+            let productById = product.showById(req.params, req.query);
+            res.status(productById.status).json(productById.response);
+        }catch(er){}
+
+        // let { body, params, query } = req;
+        // console.log({ body, params, query });
+        // res.status(200).json({ ok: true, http: 'OK' });
     });
     
     server.post('/api/products', (req, res) => {
--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const { API_PORT } = require('./config/consts.js');
 
 const routes = require('./app/route');
 routes(server);
+server.use((req, res) => res.json(404).json({ errorText: "Page not found." }));
 
 server.listen(API_PORT, er => console.log(er || `Server on port ${API_PORT}`))
